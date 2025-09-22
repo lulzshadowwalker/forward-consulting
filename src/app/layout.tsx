9601f156cee06efa116c122447987d8b7fe4fd3d
lulzshadowwalker <lulzshadowwalker@gmail.com>
2025-09-22 09:01:07 +0300

    feat: add Google Analytics integration and update dependencies for improved tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { ReactNode } from "react";
 
 type Props = {
@@ -7,5 +8,13 @@ type Props = {
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
 export default function RootLayout({ children }: Props) {
-  return children;
+  return (
+    <>
+      {children}
+      <GoogleAnalytics
+        gaId="G-W6XV7DGTXF"
+        debugMode={process.env.NODE_ENV !== "production"}
+      />
+    </>
+  );
 }
